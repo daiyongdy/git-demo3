8f5cdeae89c0b5747dbc5cc56a054ed668828dc2
dy <844723655@qq.com>
2015-05-12 15:25:18 +0800

    main第一次提叫

---
+++ src/com/daiyong/git3/GitTest3.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015 智联招聘-Zhaopin.com
+ * 
+ * From project git-demo3
+ */
+
+package com.daiyong.git3;
+
+/**
+ * @author 代勇<yong.dai@zhaopin.com.cn>
+ * @Date 2015-5-12
+ */
+public class GitTest3 {
+	public static void main(String[] args) {
+		
+	}
+}
